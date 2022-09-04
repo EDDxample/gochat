cc57b4ce1ed15ab43631bfe6188b8efc3756847c
EDDxample <EDDxample@gmail.com>
2022-09-05 01:01:23 +0200

    unwrap comments

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -15,8 +15,7 @@ type Server struct {
 	leavingClients chan chan string
 }
 
-// NewServer returns a new Server object
-// and initializes the listener channels
+// NewServer returns a new Server object and initializes the listener channels
 func NewServer(host string, port int) *Server {
 	return &Server{
 		host:           host,
@@ -27,8 +26,7 @@ func NewServer(host string, port int) *Server {
 	}
 }
 
-// Run starts listening on the assigned port
-// and bootstraps the message and connection handlers
+// Run starts listening on the assigned port and bootstraps the message and connection handlers
 func (s *Server) Run() {
 	fmt.Printf("Listening on port %d...\n", s.port)
 
@@ -48,8 +46,8 @@ func (s *Server) Run() {
 	}
 }
 
-// HandleUserMessages keeps track of joining/leaving clients
-// and broadcasts the new messages to all the active listeners
+// HandleUserMessages keeps track of joining/leaving clients and broadcasts the new messages to
+// all the active listeners
 func (s *Server) HandleUserMessages() {
 	activeClients := make(map[chan string]bool)
 
@@ -75,9 +73,8 @@ func (s *Server) HandleUserMessages() {
 	}
 }
 
-// HandleConnection adds a new client to the list
-// of new joins and connects the input and output messages
-// to their respective targets
+// HandleConnection adds a new client to the list of new joins and connects the input and output
+// messages to their respective targets
 func (s *Server) HandleConnection(conn net.Conn) {
 	defer conn.Close()
 	clientChannel := make(chan string)
@@ -106,22 +103,19 @@ func (s *Server) HandleConnection(conn net.Conn) {
 	fmt.Printf(leaveMsg)
 }
 
-// ProcessOutboundMessages writes the registered messages
-// from the given channel to the client connection
+// ProcessOutboundMessages writes the registered messages from the given channel to the client connection
 func (s *Server) ProcessOutboundMessages(conn net.Conn, clientChannel <-chan string) {
 	for message := range clientChannel {
 		fmt.Fprintln(conn, message)
 	}
 }
 
-// SendToServer registers the given text
-// to be sent to the active clients
+// SendToServer registers the given text to be sent to the active clients
 func (s *Server) SendToServer(text string) {
 	s.chatMessages <- text
 }
 
-// SendToClient registers the given text
-// to be sent to the given client channel
+// SendToClient registers the given text to be sent to the given client channel
 func (s *Server) SendToClient(text string, clientChannel chan string) {
 	clientChannel <- text
 }
