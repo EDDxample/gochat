8342562af411c423c3340cf5ce2469adcb93a7a0
EDDxample <EDDxample@gmail.com>
2022-09-05 00:39:24 +0200

    add server bootstrap

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"flag"
+
+	"github.com/EDDxample/gochat/pkg/server"
+)
+
+var (
+	host = flag.String("h", "localhost", "host")
+	port = flag.Int("p", 3090, "port")
+)
+
+func main() {
+	flag.Parse()
+	server := server.NewServer(*host, *port)
+	server.Run()
+}
