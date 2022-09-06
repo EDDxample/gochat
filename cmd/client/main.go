2e34ed67ea7c7b7e2d30168aa1059b0193763c37
EDDxample <EDDxample@gmail.com>
2022-09-06 22:07:31 +0200

    add client logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"flag"
+
+	"github.com/EDDxample/gochat/pkg/client"
+)
+
+var (
+	host = flag.String("h", "localhost", "host")
+	port = flag.Int("p", 3090, "port")
+)
+
+func main() {
+	flag.Parse()
+	client := client.NewClient(*host, *port)
+	client.Run()
+}
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,1 +1,45 @@
 package client
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+// Client implementation for the TCP socket chat
+type Client struct {
+	host string
+	port int
+}
+
+// NewClient returns a new Client object
+func NewClient(host string, port int) *Client {
+	return &Client{
+		host: host,
+		port: port,
+	}
+}
+
+// Run connects to the server and starts routing messages
+func (c *Client) Run() {
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", c.host, c.port))
+	if err != nil {
+		panic(err)
+	}
+
+	done := make(chan bool)
+
+	go func() {
+		io.Copy(os.Stdout, conn)
+		done <- true
+	}()
+
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	<-done
+
+}
